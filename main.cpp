a47246f586ebc14962ce9c03613845a96a901b04
EmyCodez <130977482+EmyCodez@users.noreply.github.com>
2025-08-17 12:01:56 +0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "PmergeMe.hpp"
+
+int main(int argc, char** argv)
+{
+	PmergeMe sorter;
+	if (sorter.processInput(argc, argv) != 0)
+		return 1;
+
+	std::vector<int>& vec = sorter.getVector();
+	std::list<int>& lst = sorter.getList();
+
+	sorter.performMergeSortVector(vec);
+	sorter.performMergeSortList(lst);
+
+	return 0;
+}
