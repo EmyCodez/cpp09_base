e4714c621bf090878a1cd714032dfafa5e4c6bba
EmyCodez <130977482+EmyCodez@users.noreply.github.com>
2025-08-17 16:04:20 +0400

    Update PmergeMe.hpp

--- PmergeMe.hpp
+++ PmergeMe.hpp
@@ -5,45 +5,43 @@
 #include <sstream>
 #include <vector>
 #include <list>
-#include <chrono>
 #include <algorithm>
 #include <set>
+#include <ctime>  // For timing
+#include <cstdlib> // For atoi
 
 #define MAX_LIMIT 3000
 
 class PmergeMe
 {
 private:
-	std::vector<int> vector;
-	std::list<int> list;
+    std::vector<int> vecNumbers;
+    std::list<int> lstNumbers;
+    double vectorDuration;  // microseconds
+    double listDuration;    // microseconds
 
-	std::chrono::duration<double, std::micro> vectorDuration;
-	std::chrono::duration<double, std::micro> listDuration;
-
-	// Sorting helpers
-	void fordJohnsonVector(std::vector<int>& data);
-	void fordJohnsonList(std::list<int>& data);
-	std::vector<int> generateJacobsthalSequence(int n);
+    // Sorting helpers
+    void fordJohnsonVector(std::vector<int>& data);
+    void fordJohnsonList(std::list<int>& data);
+    std::vector<int> generateJacobsthalSequence(int n);
 
 public:
-	PmergeMe();
-	PmergeMe(const PmergeMe& other);
-	PmergeMe& operator=(const PmergeMe& other);
-	~PmergeMe();
+    PmergeMe();
+    PmergeMe(const PmergeMe& other);
+    PmergeMe& operator=(const PmergeMe& other);
+    ~PmergeMe();
 
-	int processInput(int argc, char** argv);
+    int processInput(int argc, char** argv);
 
-	void performMergeSortVector(std::vector<int>& vec);
-	void performMergeSortList(std::list<int>& lst);
+    void performMergeSortVector(std::vector<int>& vec);
+    void performMergeSortList(std::list<int>& lst);
 
-	void printVector(std::string word);
-	void printList();
+    void printVector(const std::string& word);
+    void printList();
 
-	// Setters & Getters
-	void setVector(std::vector<int>& vector);
-	void setList(std::list<int>& list);
-	std::vector<int>& getVector();
-	std::list<int>& getList();
+    // Accessors
+    std::vector<int>& getVector();
+    std::list<int>& getList();
 };
 
 #endif
